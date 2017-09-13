8d065dad07527909e2cbc53cfcbfb30746c7c5a6
AJ Henley <ajhenley@gmail.com>
2017-09-13 01:40:23 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SecurityStepsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityStepsApplication.class, args);
+	}
+}
