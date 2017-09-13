f2653dfa4b041aae027d0c7e3ce3e84cd30ec2e0
AJ Henley <ajhenley@gmail.com>
2017-09-13 04:51:15 -0400

    Added code to allow for stored user detail information

--- src/main/java/com/example/demo/ui/controller/HomeController.java
+++ src/main/java/com/example/demo/ui/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.example.demo.ui.controller;
 
+import com.example.demo.entity.User;
+import com.example.demo.security.CustomUserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
@@ -15,7 +23,9 @@ public class HomeController {
     }
 
     @RequestMapping("/secure")
-    public String secure(){
+    public String secure(Principal principal, Model model){
+        User myuser = ((CustomUserDetails) ((UsernamePasswordAuthenticationToken) principal).getPrincipal()).getUser();
+        model.addAttribute("myuser", myuser);
         return "secure";
     }
 }
